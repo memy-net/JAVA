b74d8cdf236d16dbe5f9781a9adb4fab0c6c3351
memy-net <43229078+memy-net@users.noreply.github.com>
2018-10-20 18:06:46 +0200

    Create 1992.js

---
+++ 1992.js
@@ -0,0 +1,1 @@
+document.onkeypress=function(n){return 123==(n=n||window.event).keyCode?!1:void 0},document.onmousedown=function(n){return 123==(n=n||window.event).keyCode?!1:void 0},document.onkeydown=function(n){return 123==(n=n||window.event).keyCode?!1:void 0};
