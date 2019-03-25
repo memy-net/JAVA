913d3fcd407d5472c18576eea3efd652b4c8e23c
memy-net <43229078+memy-net@users.noreply.github.com>
2019-03-25 01:31:28 +0000

    Delete 1992.js

--- 1992.js
+++
@@ -1,1 +0,0 @@
-document.onkeypress=function(n){return 123==(n=n||window.event).keyCode?!1:void 0},document.onmousedown=function(n){return 123==(n=n||window.event).keyCode?!1:void 0},document.onkeydown=function(n){return 123==(n=n||window.event).keyCode?!1:void 0};
